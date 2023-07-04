862eb00fd5d780d9f58aa67dfca943731df782ce
Gabriel Freitas <gabrielfreisan@gmail.com>
2023-07-04 20:39:52 -0300

    vercel

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/prop-types */
 import styles from './Avatar.module.css'
 
-export function Avatar(props){
-    const hasBorder = props.hasBorder != false;
 
-    return(
-        <img className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
-        src={props.src} />
-    );
+export function Avatar({hasBorder = true , src}) {
+
+    return (
+        <img className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+            src={src}
+        />
+    )
 }
