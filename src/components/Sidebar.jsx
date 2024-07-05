a481a940e1bab25905b43b6d0b278d8e21d39e8f
Gabriel Freitas <gabriel@clubcupons.com.br>
2024-07-05 13:36:51 -0300

    barralateral

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,11 +8,11 @@ export function Sidebar(){
         <aside className={styles.sidebar}>
         <img 
         className={styles.cover}
-        src="https://instagram.ffor3-2.fna.fbcdn.net/v/t51.2885-19/414717906_907040287468457_2851506861703981290_n.jpg?_nc_ht=instagram.ffor3-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=fOs9VBY4XskQ7kNvgFljHYi&edm=AEhyXUkBAAAA&ccb=7-5&oh=00_AYBtUX6Ouv8PNWA4uzYdR_B5Wmkb6s7JkFc2lzPCh4DidQ&oe=668DD3FE&_nc_sid=8f1549"
+        src="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40"
         />
         
         <div className={styles.profile}>
-            <Avatar src="https://img.freepik.com/fotos-gratis/retrato-de-jovem-modelo-feminino_23-2149084943.jpg?t=st=1715975536~exp=1715979136~hmac=b4ee00fb05ed136015e69ffa074327fdbf9c6f22ed520e372c4fe7ce04395fb1&w=360"/>
+            <Avatar src="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40"/>
         
         <strong>Thaís Rodrigues</strong>
         <span>Professora</span>
