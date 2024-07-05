ff25265497c26c9d114193524ae5549cb479e7d3
Gabriel Freitas <gabriel@clubcupons.com.br>
2024-07-05 13:55:26 -0300

    Plano Pro

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ export function Sidebar(){
             <Avatar src="https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40"/>
         
         <strong>Thaís Rodrigues</strong>
-        <span>Professora</span>
+        <span>Professora - PRO</span>
         </div>
 
         <footer>
